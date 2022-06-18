dca783035bff41f8c0feb59492b315bf1e22ae7a
MrBasset <daniel.pool@gmail.com>
2022-06-18 14:55:46 -0400

    Current changes - broken because of issues with the Eventually library

--- Sprinkles.ino
+++ Sprinkles.ino
@@ -6,12 +6,18 @@
 #include <DallasTemperature.h>
 
 #include <Eventually.h>
-#include <ArduinoJson.h>
+#include <IniFile.h>
 
 #include <math.h>
 #include <String.h>
 
-#include <SoftwareReset.h>
+#define SD_SELECT 4
+#define ETHERNET_SELECT 10
+
+//Digital Pins 6-9 will be used for our water zones solenoids. We can overflow to Analog pins A0 to A3 for further
+const uint8_t solenoidPins[8] = { 6, 7, 8, 9, A0, A1, A2, A3 };
+
+const int MAX_WATER_ZONES = 8;
 
 /** Data Only Entities **/
 typedef struct WaterZone
@@ -26,7 +32,6 @@ typedef struct WaterZone
 typedef struct
 {
   int DutyCycleSeconds = 30 * 60;         //30 minutes
-  int NumberOfWaterZones = 4;             //four water zones
   int StartingDurationSeconds = 180;      //three minutes
   int MinimumThresholdTempCelcius = 21;   //the temperature at which the zone starts spraying
   int MaximumThresholdTempCelcius = 27;   //the temperature at which the zone will spray continuously for it's section of the duty cycle
@@ -35,6 +40,25 @@ typedef struct
 Configuration pConf;
 
 // ---------- END DOE -----------
+
+// ---------- Memory Debugging -------
+#ifdef __arm__
+// should use uinstd.h to define sbrk but Due causes a conflict
+extern "C" char* sbrk(int incr);
+#else  // __ARM__
+extern char *__brkval;
+#endif  // __arm__
+
+int freeMemory() {
+  char top;
+#ifdef __arm__
+  return &top - reinterpret_cast<char*>(sbrk(0));
+#elif defined(CORE_TEENSY) || (ARDUINO > 103 && ARDUINO != 151)
+  return &top - __brkval;
+#else  // __arm__
+  return __brkval ? &top - __brkval : &top - __malloc_heap_start;
+#endif  // __arm__
+}
 
 // ---------- RGB Status LED ---------
 
@@ -53,129 +77,134 @@ Configuration pConf;
 // ---------- END RGB Status LED -----
 
 //----------- BEGIN Configuration --------------------
-void ReadConfigurationFromSdCard()
+void printErrorMessage(uint8_t e, bool eol = true)
 {
-  const String FILE_PATH = "/mnt/sd/arduino/settings.json";
-  const byte bufferSize = 150; //define number of bytes expected on the JSON thread
-  char json[bufferSize]; //create a char array buffer that is 150 bytes in size in this case
-
-  File dataFile = SD.open(FILE_PATH, FILE_READ);
-  dataFile.readBytes(json, bufferSize);
-  dataFile.close();
-
-  StaticJsonBuffer<JSON_OBJECT_SIZE(4)> jsonBuffer;  //We know our JSON string just has 4 objects only and no arrays, so our buffer is set to handle that many
-  JsonObject& root = jsonBuffer.parseObject(json);    // breaks the JSON string into individual items, saves into JsonObject 'root'
-
-  if (!root.success()) {
-    Serial.println(F("parseObject() failed"));
-    return;   //if parsing failed, this will exit the function and not change the values to 00
+  switch (e) {
+  case IniFile::errorNoError:
+    Serial.print(F("no error"));
+    break;
+  case IniFile::errorFileNotFound:
+    Serial.print(F("file not found"));
+    break;
+  case IniFile::errorFileNotOpen:
+    Serial.print(F("file not open"));
+    break;
+  case IniFile::errorBufferTooSmall:
+    Serial.print(F("buffer too small"));
+    break;
+  case IniFile::errorSeekError:
+    Serial.print(F("seek error"));
+    break;
+  case IniFile::errorSectionNotFound:
+    Serial.print(F("section not found"));
+    break;
+  case IniFile::errorKeyNotFound:
+    Serial.print(F("key not found"));
+    break;
+  case IniFile::errorEndOfFile:
+    Serial.print(F("end of file"));
+    break;
+  case IniFile::errorUnknownError:
+    Serial.print(F("unknown error"));
+    break;
+  default:
+    Serial.print(F("unknown error value"));
+    break;
   }
-  else 
-  {
-    // update variables
-    pConf.DutyCycleSeconds = root["DutyCycleSeconds"];
-    pConf.NumberOfWaterZones = root["NumberOfWaterZones"];
-    pConf.StartingDurationSeconds = root["StartingDurationSeconds"];
-    pConf.MinimumThresholdTempCelcius = root["MinimumThresholdTempCelcius"];
-    pConf.MaximumThresholdTempCelcius = root["MaximumThresholdTempCelcius"];
-  }
+  if (eol)
+    Serial.println();
 }
 
-void WriteConfigurationToSdCard(Configuration configuration)
+
+void ReadConfigurationFromSdCard()
 {
-  softwareReset(STANDARD);
+  const size_t bufferLen = 80;
+  char buffer[bufferLen];
+
+  const char *filename = "/settings.ini";
+
+  //initialise the SD card
+  SPI.begin();
+  if (!SD.begin(SD_SELECT)) {
+    Serial.println(F("SD initialization failed!"));
+    while (1);
+  }
+
+  IniFile ini(filename);
+  if (!ini.open()) {
+    Serial.print(F("Ini file "));
+    Serial.print(filename);
+    Serial.println(F(" does not exist"));
+    // Cannot do anything else
+    while (1);
+  }
+  Serial.println("Ini file exists");
+
+  // Check the file is valid. This can be used to warn if any lines
+  // are longer than the buffer.
+  if (!ini.validate(buffer, bufferLen)) {
+    Serial.print(F("ini file "));
+    Serial.print(ini.getFilename());
+    Serial.print(F(" not valid: "));
+    printErrorMessage(ini.getError());
+    // Cannot do anything else
+    while (1);
+  }
   
-	return;
+  Serial.println(F("Fetching Config Values"));
+
+  // Fetch DutyCycleSeconds
+  bool found = ini.getValue("Global", "DutyCycleSeconds", buffer, bufferLen, pConf.DutyCycleSeconds);
+  if (found) {
+    Serial.print(F("The value of 'DutyCycleSeconds' in section 'Global' is "));
+    // Print value, converting boolean to a string
+    Serial.println(pConf.DutyCycleSeconds);
+  }
+  else {
+    Serial.print(F("Could not get the value of 'DutyCycleSeconds' in section 'Global': "));
+    printErrorMessage(ini.getError());
+  }
+  
+  // Fetch StartingDurationSeconds
+  found = ini.getValue("Global", "StartingDurationSeconds", buffer, bufferLen, pConf.StartingDurationSeconds);
+  if (found) {
+    Serial.print(F("The value of 'StartingDurationSeconds' in section 'Global' is "));
+    // Print value, converting boolean to a string
+    Serial.println(pConf.StartingDurationSeconds);
+  }
+  else {
+    Serial.print(F("Could not get the value of 'StartingDurationSeconds' in section 'Global': "));
+    printErrorMessage(ini.getError());
+  }
+
+  // Fetch MinimumThresholdTempCelcius
+  found = ini.getValue("Global", "MinimumThresholdTempCelcius", buffer, bufferLen, pConf.MinimumThresholdTempCelcius);
+  if (found) {
+    Serial.print(F("The value of 'MinimumThresholdTempCelcius' in section 'Global' is "));
+    // Print value, converting boolean to a string
+    Serial.println(pConf.MinimumThresholdTempCelcius);
+  }
+  else {
+    Serial.print(F("Could not get the value of 'MinimumThresholdTempCelcius' in section 'Global': "));
+    printErrorMessage(ini.getError());
+  }
+
+  // Fetch MaximumThresholdTempCelcius
+  found = ini.getValue("Global", "MaximumThresholdTempCelcius", buffer, bufferLen, pConf.MaximumThresholdTempCelcius);
+  if (found) {
+    Serial.print(F("The value of 'MaximumThresholdTempCelcius' in section 'Global' is "));
+    // Print value, converting boolean to a string
+    Serial.println(pConf.MaximumThresholdTempCelcius);
+  }
+  else {
+    Serial.print(F("Could not get the value of 'MaximumThresholdTempCelcius' in section 'Global': "));
+    printErrorMessage(ini.getError());
+  }
 }
 
 // ----------- END Configuration
 
-// ----------- BEGIN JSON REST API -------------------
-
-/**
- * Accepts an inbound HTTP request and translates this into the
- * relevent operations.
- **/
-//void HandleRequest(EthernetClient& client) {
-//  
-//  bool currentLineIsBlank = true;
-//  
-//  while (client.connected()) {
-//    if (client.available()) {
-//      char c = client.read();
-//      if (c == '\n' && currentLineIsBlank) {
-//        return true;
-//      } else if (c == '\n') {
-//        currentLineIsBlank = true;
-//      } else if (c != '\r') {
-//        currentLineIsBlank = false;
-//      }
-//    }
-//  }
-//  return false;
-//}
-
-//void ReturnError(EthernetClient& client, JsonObject& json) {
-//  client.println("HTTP/1.1 400 Bad Request");
-//  client.println("Content-Type: application/json");
-//  client.println("Connection: close");
-//  client.println();
-//  client.println("{ \"error\" : \"Unknown Function\" }");
-//}
-//
-//void writeResponse(EthernetClient& client, JsonObject& json) {
-//  client.println("HTTP/1.1 200 OK");
-//  client.println("Content-Type: application/json");
-//  client.println("Connection: close");
-//  client.println();
-//
-//  json.prettyPrintTo(client);
-//}
-
-//NOTE: Example JSON Response
-JsonObject& prepareResponse(JsonBuffer& jsonBuffer) {
-  JsonObject& root = jsonBuffer.createObject();
-
-  JsonArray& analogValues = root.createNestedArray("analog");
-  for (int pin = 0; pin < 6; pin++) {
-    int value = analogRead(pin);
-    analogValues.add(value);
-  }
-
-  JsonArray& digitalValues = root.createNestedArray("digital");
-  for (int pin = 0; pin < 14; pin++) {
-    int value = digitalRead(pin);
-    digitalValues.add(value);
-  }
-
-  return root;
-}
-
-void GetConfiguration() {}
-void SetConfiguration(String request) {}
-
-void GetAllZoneSummaries(){}
-void GetZoneSummary(String request){}
-
-void OverrideCloseValve(int valve){}
-void OverrideOpenValve(int valve){}
-void GetValveStatus(int valve){}
-
-
-//------ BEGIN Event management
-
-
-void HandleTemperatureThreasholdExceeded(){}
-
-void HandleSprayTimerStarted(){}
-void HandleSprayTimerExpired(){}
-
-void HandleConfigurationChange(){}
-
 // ------- BEGIN SETUP --------------
-
-//byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};  //TODO: set this to the MAC on the Ethernet sheild
-//EthernetServer Server(80);
 
 EvtManager mgr;
 
@@ -192,14 +221,47 @@ int ActiveWaterZones = 0;
 
 void setup() {
 
+  // Configure all of the SPI select pins as outputs and make SPI
+  // devices inactive, otherwise the earlier init routines may fail
+  // for devices which have not yet been configured.
+  pinMode(SD_SELECT, OUTPUT);
+  digitalWrite(SD_SELECT, HIGH); // disable SD card
+  
+  pinMode(ETHERNET_SELECT, OUTPUT);
+  digitalWrite(ETHERNET_SELECT, HIGH); // disable Ethernet
+
+  //first lets make sure all our valves are closed
+  for( int i = 0; i < 7; i++ ) {
+    pinMode(solenoidPins[i], OUTPUT);
+    CloseValve(solenoidPins[i]);
+  }
+
+  //wait for serial to attach
+  while (!Serial);
+
   Serial.begin(9600);       // for diagnostics
 
+  Serial.println("");
+  Serial.println("");
+  Serial.print(F("****** Starting Program *********"));
+  Serial.println("");
+  Serial.println("");
+
   //load the configuration from the file system (or create the default if it doesn't exist)
+  ReadConfigurationFromSdCard();
 
   //Detect the temperature sensors
   discoverOneWireDevices();
 
-  Serial.println(
+  Serial.print(F("No. of ActiveWater Zones discovered: "));
+  Serial.println(ActiveWaterZones);
+
+  if(ActiveWaterZones == 0) {
+    Serial.println(F("No active water zones found"));
+    while(1);
+  }
+
+  Serial.println();
   Serial.print(F("Initializing Temperature Control Library Version "));
   Serial.println(DALLASTEMPLIBVERSION);
 
@@ -211,53 +273,59 @@ void setup() {
     sensors.setResolution(pWaterZones[i].Probe, 10);  // set the resolution to 10 bit (Can be 9 to 12 bits .. lower is faster)
   }
   
-  //start up the web server and set to listen on port 80
-  //if (Ethernet.begin(mac) == 0)
-  //{
-    //Serial.println("Failed to configure ethernet using DHCP");
-    //TODO: DHCP error code
-  //}
-
-  //Server.begin(); //start web server
-  
   Serial.println(F("initialising the event listeners & timers"));
   mgr.resetContext(); 
-  
-  //use a timer to poll the web server for requests every 500 milliseconds
-  //mgr.addListener(new EvtTimeListener(500, true, (EvtAction)PollServer));
+
+
+  Serial.print(F("Free Memory: "));
+  Serial.println(freeMemory());
 
   Serial.println(F("Initialise temperator sensor poll for every 2 seconds."));
   mgr.addListener(new EvtTimeListener(2000, true, (EvtAction)PollTemperatureSensors));
 
-  Serial.println(F("and give them time to stablise (approx 5 seconds) before starting the dutycycle"));
-  mgr.addListener(new EvtTimeListener(5000, false, (EvtAction)SetupDutyCycleLoop()));
+  Serial.println(F("and give them time to stablise (approx 4000 * MAX_WATER_ZONES seconds) before starting the dutycycle"));
+  mgr.addListener(new EvtTimeListener(4000 * MAX_WATER_ZONES, false, (EvtAction)SetupDutyCycleLoop));
 }
 
 bool PollTemperatureSensors()
 {
-  Serial.print("Getting temperatures... ");  
+  Serial.print(F("Getting temperatures... "));
   Serial.println();
+
+  Serial.print(F("Free Memory: "));
+  Serial.println(freeMemory());
 
   // Command all devices on bus to read temperature  
   sensors.requestTemperatures(); 
 
   for(int i = 0; i < ActiveWaterZones; i++)
   {
+    Serial.print(F("Fetching temp for probe "));
+    Serial.println(i);
+
+
+  Serial.print(F("Free Memory: "));
+  Serial.println(freeMemory());
+    
     float tempC = sensors.getTempC(pWaterZones[i].Probe);
     if (tempC == -127.00) 
     {
-      Serial.print("Error getting temperature  ");
+      Serial.println(F("Error getting temperature  "));
       pWaterZones[i].IsProbeError = true;
     }
     else
     {
       Serial.print("C: ");
-      Serial.print(tempC);
+      Serial.println(tempC);
 
       pWaterZones[i].IsProbeError = false;
       pWaterZones[i].CurrentTemperature = tempC;
     }
   }
+
+  
+  Serial.print(F("Free Memory: "));
+  Serial.println(freeMemory());
 
   //stop the current action chain
   return false;
@@ -274,16 +342,28 @@ bool PollTemperatureSensors()
  */
 bool SetupDutyCycleLoop()
 {
+  Serial.print(F("Free Memory: "));
+  Serial.println(freeMemory());
+  
+  Serial.println(F("Setting up duty cycle loop"));
   float zoneInterval = pConf.DutyCycleSeconds / ActiveWaterZones; //The amout of time (in seconds) each zone gets
+
+  Serial.print(F("Zone interval is :"));
+  Serial.println(zoneInterval);
 
   for (int i = 0; i < ActiveWaterZones; i++)
   {
+
+
+    Serial.print(F("Free Memory: "));
+    Serial.println(freeMemory());
+    
     //establish when a zone starts its section of the Duty Cycle
-    mgr.addListener(new EvtTimeListener(i * zoneInterval, true, (EvtAction)ProcessZone(i, zoneInterval)));
+    mgr.addListener(new EvtTimeListener(i * zoneInterval * 1000, true, (EvtAction)ProcessZone(i, zoneInterval)));
   }
 
   //continue the event chain with the new listeners we've added.
-  return true;
+  return false;
 }
 
 bool ProcessZone(int zone, float maximumInterval)
@@ -306,11 +386,11 @@ bool ProcessZone(int zone, float maximumInterval)
       if (openseconds > maximumInterval) openseconds = maximumInterval;
     }
 
-    Serial.print("Switching zone ");
+    Serial.print(F("Switching zone "));
     Serial.print(zone);
-    Serial.print(" on for ");
+    Serial.print(F(" on for "));
     Serial.print(openseconds);
-    Serial.println(" seconds");
+    Serial.println(F(" seconds"));
 
     //Open the valve
     OpenValve(pWaterZones[zone].Solenoid);
@@ -325,15 +405,14 @@ bool ProcessZone(int zone, float maximumInterval)
   return false;
 }
 
-bool PollServer()
-{
-//  EthernetClient client = Server.available();
-//  if (client) {
-//    HandleRequest(client);
-//  }
-
-  //all done here
-  return false;
+void DebugProbeAddress (DeviceAddress address) {
+  //some output debug, not strictly necessary
+  for(int i = 0; i < 8; i++) {
+    Serial.print(F("0x"));
+    if (address[i] < 16) Serial.print('0');
+    Serial.print(address[i], HEX);
+    if (i < 7) Serial.print(F(", "));
+  }
 }
 
 /*
@@ -342,51 +421,46 @@ bool PollServer()
  */
 void discoverOneWireDevices() {
 
-  DeviceAddress addresses[8] = { 0 };
+  DeviceAddress addresses[MAX_WATER_ZONES] = { 0 };
   int row = 0;
   
-  Serial.print("Looking for 1-Wire devices...\n\r");
+  Serial.print(F("Looking for 1-Wire devices...\n\r"));
   while(oneWire.search(addresses[row])) {
-    Serial.print("\n\r\n\rFound \'1-Wire\' device with address:\n\r");
-
-    //some output debug, not strictly necessary
-    for(int i = 0; i < 8; i++) {
-      Serial.print("0x");
-      if (addresses[row][i] < 16) Serial.print('0');
-      Serial.print(addresses[row][i], HEX);
-      if (i < 7) Serial.print(", ");
-    }
+    Serial.print(F("\n\r\n\rFound \'1-Wire\' device with address:\n\r"));
+    DebugProbeAddress(addresses[row]);
 
     ActiveWaterZones++;
-
-    if (row == 7) break;
+    
+    if (row == MAX_WATER_ZONES - 1) break;
     row++;
   }
   
   Serial.println();
-  Serial.print("Done");
+  Serial.println(F("Done"));
   oneWire.reset_search();
 
+  Serial.println(F("Setup WaterZones array with probe addresses"));
   //now initialise WaterZones as an array of the correct size (only done in setup to avoid heap fragmentation).
   if(pWaterZones != 0) delete [] pWaterZones;
   pWaterZones = new WaterZone[ActiveWaterZones];
 
   int crcFailure = 0;
 
-  //Digital Pins 6-9 will be used for our water zones solenoids. We can overflow to Analog pins A0 to A3 for further
-  uint8_t solenoidPins[8] = { 9, 8, 7, 6, A0, A1, A2, A3 };
-
   //populate WaterZones
   for( int i = 0; i < ActiveWaterZones; i++) {
     
     //CRC check
     if ( OneWire::crc8( addresses[i], 7) != addresses[i][7]) {
-      Serial.print("CRC is not valid!\n\r");
+      Serial.print(F("CRC is not valid!\n\r"));
       crcFailure++;
       continue;
     }
-    //The DeviceAddress is an unsigned array of 8-bits, have to use memcpy( arrB, arrA, 5 ); to copy it.
+    //The DeviceAddress is an unsigned array of 8-bits, have to use memcpy( arrB, arrA, 8 ); to copy it.
     memcpy( pWaterZones[i].Probe, addresses[i], 8 );
+
+    Serial.println(F("Setting waterzone probe address as:"));
+    DebugProbeAddress(pWaterZones[i].Probe);
+    
     pWaterZones[i].Solenoid = solenoidPins[i];
 
     //set up pin
@@ -395,6 +469,7 @@ void discoverOneWireDevices() {
   }
 
   ActiveWaterZones = ActiveWaterZones - crcFailure;
+  Serial.println(F("All zones configured"));
 }
 
 // ---------- END SETUP --------------
